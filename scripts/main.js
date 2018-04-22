a963aab346544e698002ff2742c47cb7dd3b83b9
Roman <roman.khairov@gmail.com>
2018-04-22 16:11:54 +0200

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+var $myForm = document.getElementById('myForm');
+
+
+function displayMessage(message) {
+ // Some code
+}
+
+$myForm.addEventListener('submit', function(evt) {
+
+    evt.preventDefault();
+
+    var formData = new FormData(myForm);
+    console.log(formData);
+
+});
+
+// Test 2
